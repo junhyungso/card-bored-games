ecdcf26040ca86dcda3b717b862b8aafeb0d6133
Manogya Acharya <acharyma@bc.edu>
2020-04-17 18:59:24 -0400

    Updates

---
+++ precache-manifest.301ad2abdc883ecf312a3c24c3139d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f7a8160e76f35f09c81658aedf776c",
+    "url": "/creative-project-card-bored-games/index.html"
+  },
+  {
+    "revision": "fd1606d48b2023720449",
+    "url": "/creative-project-card-bored-games/static/css/main.fa9a8182.chunk.css"
+  },
+  {
+    "revision": "60842f893fd691f1c55a",
+    "url": "/creative-project-card-bored-games/static/js/2.6d90caf5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/creative-project-card-bored-games/static/js/2.6d90caf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd1606d48b2023720449",
+    "url": "/creative-project-card-bored-games/static/js/main.dcfb949c.chunk.js"
+  },
+  {
+    "revision": "b95e8f0cf3791794cb90",
+    "url": "/creative-project-card-bored-games/static/js/runtime-main.7028c413.js"
+  }
+]);
