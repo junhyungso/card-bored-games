8b7f0f82feea393254ef76c1541a02de01d04551
Manogya Acharya <acharyma@bc.edu>
2020-04-20 15:32:28 -0400

    random game added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -19,6 +19,15 @@ function MainPage(){
     let path_one = "./game/".concat(sorted_games[0].gameId);
     let path_two = "./game/".concat(sorted_games[1].gameId);
     let path_three = "./game/".concat(sorted_games[2].gameId);
+
+    console.log(randomNumber());
+    function randomNumber(){
+        console.log(games.length)
+        return Math.floor(Math.random() * ((games.length-1) - 0 + 1));
+    }
+
+    let randomGame = games[randomNumber()];
+
     return(
         <div>
             {console.log(currentUserId)}
@@ -26,15 +35,27 @@ function MainPage(){
             <div className={css.top}>
                 <h1>What's Hot</h1>
             </div>
-            <div className={css.topthree}>
-                <Link to={path_one} className={css.three}>{number_one.gameName}</Link>
+            <div className={css.row}>
+                <div className={css.col6}>
+                    <div className={css.topthree}>
+                        <Link to={path_one} className={css.three}>{number_one.gameName}</Link>
+                    </div>
+                    <div className={css.topthree}>
+                        <Link to={path_two} className={css.three}>{number_two.gameName}</Link>
+                    </div>
+                    <div className={css.topthree}>
+                        <Link to={path_three} className={css.three}>{number_three.gameName}</Link>
+                    </div>
+                </div>
+                <div className={css.col3}>
+                    <div className={css.dice}>
+                        <Link to={"./game/"+randomGame.gameId}>
+                            <img width="250" height="250" src={publicUrl('/assets/dice.png')}></img>
+                        </Link>
+                    </div>
+                </div>
             </div>
-            <div className={css.topthree}>
-                <Link to={path_two} className={css.three}>{number_two.gameName}</Link>
-            </div>
-            <div className={css.topthree}>
-                <Link href={path_three} className={css.three}>{number_three.gameName}</Link>
-            </div>
+            
         </div>
         
     )
